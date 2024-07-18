33169a11817ab91b6035a89ef00402a57df5fd8f
dimitris-nteams <dimitris@nteams>
2024-07-19 01:38:36 +0300

    minor og fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,59 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import "app/global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "app/components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "app/components/footer";
+import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
+  metadataBase: new URL('https://dkountanis.xyz'),
   title: {
-    default: 'Dimitris Kountanis',
-    template: '%s | Dimitris Kountanis',
-},
-description: 'Founding Engineer at Native Teams, Front-End Developer, and Fintech Enthusiast.',
-openGraph: {
-    title: 'Dimitris Kountanis | Head of Front-End at Native Teams',
-    description: 'Explore the portfolio of Dimitris Kountanis, a founding engineer at Native Teams, front-end developer, and fintech enthusiast. Discover insights on engineering management, software development, and industry trends.',
-    url: baseUrl,
-    siteName: 'Dimitris Kountanis Portfolio',
-    locale: 'en_US',
-    type: 'website',
-},
+    default: "Dimitris Kountanis",
+    template: "%s | Dimitris Kountanis",
+  },
+  description:
+    "Founding Engineer at Native Teams, Front-End Developer, and Fintech Enthusiast.",
+  openGraph: {
+    title: "Dimitris Kountanis | Head of Front-End at Native Teams",
+    description:
+      "Explore the portfolio of Dimitris Kountanis, a founding engineer at Native Teams, front-end developer, and fintech enthusiast. Discover insights on engineering management, software development, and industry trends.",
+    url: 'https://dkountanis.xyz',
+    siteName: "Dimitris Kountanis Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
   robots: {
     index: true,
     follow: true,
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-}
+  twitter: {
+    title: "Dimitris Kountanis",
+    card: "summary_large_image",
+  },
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        "text-black bg-white dark:text-white dark:bg-black",
         GeistSans.variable,
         GeistMono.variable
       )}
@@ -65,5 +71,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
