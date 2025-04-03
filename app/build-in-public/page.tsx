73dd77c1691be7a5290dea8202e084a9ce0dff0e
lalagias <jimkountanis@gmail.com>
2025-04-03 18:46:19 +0300

    Removes  heyrival.io

--- app/build-in-public/page.tsx
+++ app/build-in-public/page.tsx
@@ -30,18 +30,6 @@ export default async function Page() {
           </span>
           <Badge className="ml-auto">On going</Badge>
         </Link>
-        <Link
-          href="https://heyrival.io"
-          className="flex items-center space-y-1 mb-4 border rounded-md p-5 border-neutral-400 hover:bg-neutral-100 dark:hover:bg-neutral-800 transition-colors"
-        >
-          <p className="text-lg font-semibold">HeyRival</p>
-          <span className="text-sm text-neutral-600 dark:text-neutral-400 ml-2">
-            heyrival.io
-          </span>
-          <Badge className="ml-auto" variant="secondary">
-            Pre-launch
-          </Badge>
-        </Link>
       </div>
     </section>
   );
