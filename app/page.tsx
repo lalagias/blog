f9aaa52d6f2c21b0c9ed934017f5ee9b348715d2
lalagias <jimkountanis@gmail.com>
2026-01-05 02:16:19 +0200

    Adds new change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import Link from "next/link";
-import Image from "next/image";
-
-import { BlogPosts } from "@/app/components/posts";
-import NTLogo from "@/app/assets/icons/logo-monogram.svg";
-import DimitrisPortrait from "@/public/dimitris-photo.jpg";
+import Image from "next/image"
+import Link from "next/link"
+import NTLogo from "@/app/assets/icons/logo-monogram.svg"
+import { BlogPosts } from "@/app/components/posts"
+import DimitrisPortrait from "@/public/dimitris-photo.jpg"
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hey, I'm Dimitris 🏄🏼
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Hey, I'm Dimitris 🏄🏼</h1>
 
       <Image
         src={DimitrisPortrait}
@@ -25,27 +22,21 @@ export default function Page() {
           href="https://nativeteams.com/"
           className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
         >
-          <Image
-            src={NTLogo}
-            alt="Native Teams Logo"
-            width="12"
-            height="12"
-            className="mr-1"
-          />
+          <Image src={NTLogo} alt="Native Teams Logo" width="12" height="12" className="mr-1" />
           Native Teams
         </Link>
-        , where I help build the future of work. From HR to payroll solutions,
-        we're creating a platform that supports the new way of working.
+        , where we build EoR as our main service, along with HR to payroll solutions. We're
+        implementing work & payments infrastructure for global teams.
       </p>
 
       <p className="mb-4">
-        This is all new for me. Writing and sharing my thoughts with the world.
-        I'll be sharing my mind about engineering management, software, fintech
-        and whatever else pops. Dive in and enjoy the read!
+        This is all new for me. Writing and sharing my thoughts with the world. I'll be sharing my
+        mind about engineering management, software, fintech and whatever else pops. Dive in and
+        enjoy the read!
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  );
+  )
 }
