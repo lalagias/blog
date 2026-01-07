6b8d1fa33903b34df410b8bd516d82ff4a76fa6c
lalagias <jimkountanis@gmail.com>
2026-01-07 14:05:07 +0200

    Adds minor change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
-import NTLogo from "@/assets/icons/logo-monogram.svg"
 import { BlogPosts } from "@/components/posts"
+import NTLogo from "@/public/assets/icons/logo-monogram.png"
 import DimitrisPortrait from "@/public/dimitris-photo.jpg"
 
 export default function Page() {
@@ -20,9 +20,9 @@ export default function Page() {
         {`I'm a front-end developer and fintech enthusiast. Head of Frontend at`}{" "}
         <Link
           href="https://nativeteams.com/"
-          className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+          className="inline-flex items-center border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
         >
-          <Image src={NTLogo} alt="Native Teams Logo" width="12" height="12" className="mr-1" />
+          {/* <Image src={NTLogo} alt="Native Teams Logo" width="12" height="12" className="mr-1 w-4 h-4 object-contain" /> */}
           Native Teams
         </Link>
         , where we build EoR as our main service, along with HR to payroll solutions. We're
